de464cc088c97e28e49469f892f8d319f040d0cf
Luke D. Jones <luke@ljones.dev>
2021-11-03 09:07:43 +1300

    Fix: use correct path for egpu switch

--- src/lib.rs
+++ src/lib.rs
@@ -76,4 +76,4 @@ static PRIMARY_GPU_NVIDIA: &[u8] = br#"
 static PRIMARY_GPU_END: &[u8] = br#"
 EndSection"#;
 
-static EGPU_ENABLE_PATH: &str = "/sys/devices/platform/asus-nb-wmi/dgpu_disable";
+static EGPU_ENABLE_PATH: &str = "/sys/devices/platform/asus-nb-wmi/egpu_enable";
